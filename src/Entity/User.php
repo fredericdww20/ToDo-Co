4b6b09dafec05dfb0db5cf1f8985eb5f36b6848a
Frederic Portemer <synapsefred@hotmail.fr>
2024-06-19 13:52:30 +0200

    Update core

---
+++ src/Entity/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity]
+#[ORM\Table(name: "user")]
+#[UniqueEntity(fields: ["username"], message: "Ce nom d'utilisateur est déjà utilisé.")]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 25, unique: true)]
+    #[Assert\NotBlank(message: "Vous devez saisir un nom d'utilisateur.")]
+    private $username;
+
+    #[ORM\Column(type: "string", length: 64)]
+    private $password;
+
+    #[ORM\Column(type: "string", length: 60, unique: true)]
+    #[Assert\NotBlank(message: "Vous devez saisir une adresse email.")]
+    #[Assert\Email(message: "Le format de l'adresse n'est pas correct.")]
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->username;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
---
+++ src/Security/UserAuthenticator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserAuthenticator extends AbstractLoginFormAuthenticator
+{
+    private RouterInterface $router;
+    private UserProviderInterface $userProvider;
+
+    public function __construct(RouterInterface $router, UserProviderInterface $userProvider)
+    {
+        $this->router = $router;
+        $this->userProvider = $userProvider;
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->router->generate('login');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $username = $request->request->get('username', '');
+
+        if (empty($username)) {
+            throw new CustomUserMessageAuthenticationException('Nom d\'utilisateur non trouvé.');
+        }
+
+        // Vérifier si l'utilisateur existe
+        try {
+            $user = $this->userProvider->loadUserByIdentifier($username);
+        } catch (AuthenticationException $e) {
+            throw new CustomUserMessageAuthenticationException('Nom d\'utilisateur non trouvé.');
+        }
+
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($request->request->get('password', ''))
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new RedirectResponse($this->router->generate('task_list'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        return new RedirectResponse($this->router->generate('login'));
+    }
+}
