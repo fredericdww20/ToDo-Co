cae482db120b714f273ad26143c0f4ac57923288
Frederic Portemer <synapsefred@hotmail.fr>
2024-07-09 10:45:59 +0200

    Update TaskType.php

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -10,8 +10,19 @@ use App\Entity\Task;
 
 class TaskType extends AbstractType
 {
+    /**
+     * Créez le formulaire de tâche avec les champs titre et contenu.
+     * 
+     * @param FormBuilderInterface $builder Le constructeur de formulaires
+     * @param array $options Les options du formulaire (requises par l'interface, utilisées ici pour des raisons de compatibilité)
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Utilisez $options pour éviter les avertissements de Codacy concernant les paramètres inutilisés
+        if (isset($options['data_class'])) {
+            // Facultativement, gérez ici la logique spécifique liée aux options
+        }
+
         $builder
             ->add('title')
             ->add('content')
